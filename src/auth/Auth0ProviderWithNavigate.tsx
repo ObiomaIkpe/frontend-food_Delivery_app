2d224868568ff966307cc6ab31a9b50e2eaf98c7
ObiomaIkpe <obiomaikpe1.com>
2024-03-31 10:05:27 +0100

    setup user auth, protected user profile page, and landing page

---
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,45 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const Auth0ProviderWithNavigate = ({children}: Props) => {
+    const navigate = useNavigate();
+    const {createUser, } = useCreateMyUser();
+    
+    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+    const redirecturi = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+    if(!domain || !clientId || !redirecturi || !audience) {
+        throw new Error("unable to initialize auth!")
+    }
+
+    const onRedirectCallback = (appState?: AppState, user?: User) => {
+        // console.log("USER", user)
+        // console.log("TOKEN", token)
+        navigate("/auth-callback")
+
+        if(user?.sub && user?.email) {
+            createUser({
+                auth0Id: user.sub, 
+                email: user.email
+            })
+        }
+    }
+
+    return (
+        <Auth0Provider domain={domain}
+        clientId={clientId}
+        authorizationParams={{ redirect_uri: redirecturi, audience }}
+        onRedirectCallback={onRedirectCallback}
+        
+        >{children} </Auth0Provider>
+    )
+};
+
+export default Auth0ProviderWithNavigate;
---
+++ src/pages/UserProfilePage.tsx
@@ -0,0 +1,9 @@
+import UserProfileForm from '@c'
+
+const UserProfilePage = () => {
+    return (
+        <UserProfileForm />
+    )
+}
+
+export default UserProfilePage;
